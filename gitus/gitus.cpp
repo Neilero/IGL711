de7fbba327c4e007ee277ef6b9b0dac884cd94a6
Léo Pécault <leo.pecault@usherbrooke.ca>
2020-01-12 20:51:17 -0500

    Début de l'aguillage pour les commandes Début de l'affichage du help

--- gitus/gitus.cpp
+++ gitus/gitus.cpp
@@ -1,10 +1,47 @@
 #include <iostream>
 
 #include <dummy.h>
+#include <string>
 
-int main()
+using namespace std;
+
+void init()
 {
-	TestDeTrucs();
-    std::cout << GetAnswerToLifeUniverseAndEverything() << std::endl;
+    std::cout << "Vous avez lancé un init" << std::endl;
+}
+
+void add()
+{
+    std::cout << "Vous avez lancé un add" << std::endl;
+}
+
+void commit()
+{
+    std::cout << "Vous avez lancé un commit" << std::endl;
+}
+
+void help()
+{
+    std::cout << "usage: gitus <command> [<args>]" << std::endl << std::endl;
+    
+    std::cout << "These are common gitus commands used in various situations:" << std::endl;
+    std::cout << "init" << "\t" << "Create an empty Git repository or reinitialize an existing one" << std::endl;
+    std::cout << "add" << "\t" << "Add file contents to the index" << std::endl;
+    std::cout << "commit" << "\t" << "Record changes to the repository" << std::endl;
+}
+
+int main(int argc, char *argv[])
+{
+    string commande(argv[1]);
+
+	if (commande.compare("init") == 0)
+        init();
+    else if (commande.compare("add") == 0)
+        add();
+    else if (commande.compare("commit") == 0)
+        commit();
+    else
+        help();
+
     return 0;
 }
