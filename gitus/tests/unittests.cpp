ffb49b0481e13838d7bd63556876b0d2f5abf98f
farah <rebf3102@usherbrooke.ca>
2020-01-26 18:41:03 -0500

    Modified unittests.cpp file

--- gitus/tests/unittests.cpp
+++ gitus/tests/unittests.cpp
@@ -21,9 +21,6 @@ TEST_CASE("is the world ok")
 TEST_CASE("init command: everything is fine") 
 {
 	fs::path currentPath = fs::current_path();
-	if(!(fs::is_directory(currentPath.append("tests")))){
-        fs::create_directory(currentPath.append("tests"));
-	}
 
 	// If arguments: 
 		// - Checks if -h and --help parameters are ok
@@ -31,45 +28,53 @@ TEST_CASE("init command: everything is fine")
 	REQUIRE(init("--help") == true);
 		// - Checks if wrong parameters
 	REQUIRE(init("") == false);
-	REQUIRE(init(1) == false);
+	REQUIRE(init("1") == false);
 
 	// If no argument:
 		// - Checks if everything is ok and runs the init() method
 	REQUIRE(init() == true);
 	init();
-		// - Checks whether the .git and objects  directories and the index file are created or not
-	REQUIRE(fs::is_directory(currentPath.append("tests/.git")) == true);
-	REQUIRE(fs::is_directory(currentPath.append("tests/.git/objects")) == true);
-	REQUIRE(fs::exists(currentPath.append("tests/.git/index")) == true);
+		// - Checks whether the .git and objects directories and the index file are created or not
+	REQUIRE(fs::is_directory(currentPath/".git") == true);
+	REQUIRE(fs::is_directory(currentPath/".git/objects") == true);
+	REQUIRE(fs::exists(currentPath/".git/index") == true);
 
 }
 
 TEST_CASE("add command: everything is fine") 
 {
 	fs::path currentPath = fs::current_path();
-	if(!(fs::is_directory(currentPath.append("tests")))){
-        fs::create_directory(currentPath.append("tests"));
-	}
-	fs::ofstream file = fs::ofstream(currentPath.append("test.txt"));
+	std::ofstream file = std::ofstream((currentPath/"test.txt").string());
     file.close();
 	
 	// If no argument:
-	REQUIRE(add() == false);
+	//REQUIRE(add() == false);
 
 	// If wrong parameter:
-	REQUIRE(add(1) == false);
-	REQUIRE(add("") == false);
+	std::vector<std::string> args;
+	args.push_back("1");
+	args.push_back("");
+	REQUIRE(add(args) == true);
 
 	// If <pathspec> parameter is ok:
 		// - Checks if everything is ok and runs the add() method
-		REQUIRE(add("test.txt"));
-		add("test.txt");
+	args.clear();
+	args.push_back("test.txt");
+	REQUIRE(add(args));
+	add(args);
+
+	args.push_back("test.txt");
+	args.push_back("-h");
+	REQUIRE(add(args) == true);
+
+	args.clear();
+	args.push_back("test.txt");
+	args.push_back("--help");
+	REQUIRE(add(args) == false);
+
 		// - Checks whether the file is added to the staged area or not,
 		// 	that the index file is modified with the right information
 		// 	and are inserted in the objects directory as a blob
-		REQUIRE()
-
-
 	
 		
 }
