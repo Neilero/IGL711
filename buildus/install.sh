1557a344b3a814c4bd3fba52197a1e6cd886bbb5
Aurélien Vauthier <vaua2803@usherbrooke.ca>
2020-02-19 19:54:56 -0500

    Fix bug sudo not existing when root user

--- buildus/install.sh
+++ buildus/install.sh
@@ -3,14 +3,14 @@
 echo Create temp folder yamlCpp_install
 mkdir yamlCpp_install
 (
-  cd yamlCpp_install || return
+  cd yamlCpp_install || exit
 
   echo Download yaml-cpp v0.6.3
   wget https://github.com/jbeder/yaml-cpp/archive/yaml-cpp-0.6.3.tar.gz
 
   echo Extract library
   tar xzf yaml-cpp-0.6.3.tar.gz
-  cd yaml-cpp-yaml-cpp-0.6.3 || return
+  cd yaml-cpp-yaml-cpp-0.6.3 || exit
 
   echo Run CMake
   cmake .
@@ -19,8 +19,9 @@ mkdir yamlCpp_install
   make
 
   echo Run sudo make install
-  sudo make install
+  sudo make install || make install # if root user, sudo won't exist
 )
+
 echo Remove temp folder yamlCpp_install
 rm -rf yamlCpp_install
 
