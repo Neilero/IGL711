c611770fe0a74970cc0721feb9a13c59d740dd38
Léo Pécault <leo.pecault@usherbrooke.ca>
2020-04-15 03:53:30 +0200

    Added PUT and RequestMapping

--- db/src/main/java/ca/usherbrooke/dinf/dbinterface/controllers/WorkerController.java
+++ db/src/main/java/ca/usherbrooke/dinf/dbinterface/controllers/WorkerController.java
@@ -10,29 +10,52 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
+@RequestMapping(value = "/workers")
 public class WorkerController {
     @Autowired
     WorkerRepository workerRepository;
 
-    @GetMapping("/workers")
+    @GetMapping("/")
     List<Worker> getAllWorkers()
     {
         return workerRepository.findAll();
     }
 
-    @GetMapping("/workers/{id}")
+    @GetMapping("/{id}")
     Worker getWorkerById(@PathVariable UUID id)
     {
         return workerRepository.findById(id);
     }
 
-    @PostMapping("/workers")
+    @PostMapping("/")
     Worker addWorker(@RequestBody Worker newWorker)
     {
         return workerRepository.save(newWorker);
     }
 
-    @DeleteMapping("/workers/{id}")
+    @PutMapping("/{id}")
+    Worker editWorker(@RequestBody Worker newWorker, @PathVariable UUID id)
+    {
+        Worker worker = workerRepository.findById(id);
+
+        if (worker != null)
+        {
+            worker.setAddress(newWorker.getAddress());
+            worker.setPort(newWorker.getPort());
+            worker.setStatus(newWorker.getStatus());
+            worker.setPorts(newWorker.getPorts());
+            worker.setImages(newWorker.getImages());
+        }
+        else
+        {
+            worker = newWorker;
+            worker.setId(id);
+        }
+
+        return workerRepository.save(worker);
+    }
+
+    @DeleteMapping("/{id}")
     void deleteWorker(@PathVariable UUID id)
     {
         workerRepository.deleteById(id);
