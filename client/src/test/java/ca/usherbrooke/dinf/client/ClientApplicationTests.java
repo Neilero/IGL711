b26187aef4627f0d564ed2dfc3802d891115edef
Léo Pécault <leo.pecault@usherbrooke.ca>
2020-04-17 14:57:26 +0200

    Start client

---
+++ client/src/test/java/ca/usherbrooke/dinf/client/ClientApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.usherbrooke.dinf.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
