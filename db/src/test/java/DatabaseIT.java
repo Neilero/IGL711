a52b94713a967062bb6344f853eb47b711d5e4cd
Fiirouzz <farahreb@hotmail.fr>
2020-04-17 16:14:49 -0400

    PortController Tests

--- db/src/test/java/DatabaseIT.java
+++ db/src/test/java/DatabaseIT.java
@@ -12,6 +12,7 @@ import org.springframework.http.ResponseEntity;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -30,13 +31,13 @@ public class DatabaseIT {
     private Worker w2 = new Worker();
     private Worker w3 = new Worker();
 
-    private Worker generateWorker(int number, OpenPort port) {
+    private Worker generateWorker(int number) {
         Worker w = new Worker();
         UUID uuid = UUID.randomUUID();
         w.setId(uuid);
         w.setAccessPort(number);
         w.setAddress(number + "." + number + "." + number + "." + number);
-        w.setPort(port);
+        w.setAccessPort(number*111);
         w.setStatus((short) (number%2));
         return w;
     }
@@ -56,9 +57,9 @@ public class DatabaseIT {
         p2 = generatePort(2);
         p3 = generatePort(3);
 
-        w1 = generateWorker(1, 1);
-        w2 = generateWorker(2, 2);
-        w3 = generateWorker(3, 3);
+        w1 = generateWorker(1);
+        w2 = generateWorker(2);
+        w3 = generateWorker(3);
 
     }
 
@@ -84,41 +85,46 @@ public class DatabaseIT {
         ResponseEntity<Worker> re3 = trt.getForEntity(baseUri + "workers/" + w3.getId(), Worker.class);
         assertEquals(w3, re3.getBody());
 
-        Worker w1 = generateWorker(1);
-        Worker w2 = generateWorker(2);
-        Worker w3 = generateWorker(3);
-
-        ResponseEntity<Worker> re1 = trt.postForEntity(baseUri + "/", w1, Worker.class);
-        assertEquals(HttpStatus.CREATED, re1.getStatusCode());
-
-        re1 = trt.postForEntity(baseUri + "/", w2, Worker.class);
-        assertEquals(HttpStatus.CREATED, re1.getStatusCode());
-        re1 = trt.postForEntity(baseUri + "/", w3, Worker.class);
-        assertEquals(HttpStatus.CREATED, re1.getStatusCode());
-
-        ResponseEntity<ArrayList> re2 = trt.getForEntity(baseUri + "/", ArrayList.class);
-        ArrayList a = new ArrayList();
-        a.add(w1);
-        a.add(w2);
-        a.add(w3);
-        assertEquals(a, re2.getBody());
-
-        ResponseEntity<Worker> re3 = trt.getForEntity(baseUri + "/" + w3.getId(), Worker.class);
-        assertEquals(w3, re3.getBody());
-
-        trt.delete(baseUri + "/" + w3.getId());
-        re2 = trt.getForEntity(baseUri + "/", ArrayList.class);
+        trt.delete(baseUri + "workers/" + w3.getId());
+        re2 = trt.getForEntity(baseUri + "workers/", ArrayList.class);
         assertEquals(2, re2.getBody().size());
 
     }
 
     @Test
     public void portControllerIT() {
-        assertEquals(0, 0);
+        setUp();
+
+        ResponseEntity<OpenPort> re1 = trt.postForEntity(baseUri + "ports/", p1, OpenPort.class);
+        assertEquals(HttpStatus.CREATED, re1.getStatusCode());
+
+        re1 = trt.postForEntity(baseUri + "ports/", p2, OpenPort.class);
+        assertEquals(HttpStatus.CREATED, re1.getStatusCode());
+        re1 = trt.postForEntity(baseUri + "ports/", p3, OpenPort.class);
+        assertEquals(HttpStatus.CREATED, re1.getStatusCode());
+
+        ResponseEntity<ArrayList> re2 = trt.getForEntity(baseUri + "ports/", ArrayList.class);
+        ArrayList a = new ArrayList();
+        a.add(p1);
+        a.add(p2);
+        a.add(p3);
+        assertEquals(a, re2.getBody());
+
+        ResponseEntity<OpenPort> re3 = trt.getForEntity(baseUri + "ports/" + p3.getId(), OpenPort.class);
+        assertEquals(p3, re3.getBody());
+
+        trt.delete(baseUri + "ports/" + p3.getId());
+        re2 = trt.getForEntity(baseUri + "ports/", ArrayList.class);
+        assertEquals(2, re2.getBody().size());
+
+        ResponseEntity<OpenPort[]> re4 = trt.getForEntity(baseUri + "ports/worker/" + w2.getId(), OpenPort[].class);
+        assertEquals(w2.getOpenPorts(), re4.getBody());
+
     }
 
     @Test
     public void imageControllerIT() {
+
         assertEquals(0, 0);
     }
 
