d8425f60b4ff297c9c46bf12558ac6e52c44d380
Fiirouzz <farahreb@hotmail.fr>
2020-04-18 15:15:16 -0400

    Modified OrchestrusIT, added ClientIT class (not implemented yet)

--- orchestrus/src/test/java/OrchestrusIT.java
+++ orchestrus/src/test/java/OrchestrusIT.java
@@ -1,9 +1,16 @@
 import orchestrus.OrchestrusApplication;
 import orchestrus.model.DockerImage;
 import orchestrus.model.Worker;
+import orchestrus.rest.dto.BasicResponse;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest(classes = OrchestrusApplication.class)
 public class OrchestrusIT {
@@ -22,20 +29,64 @@ public class OrchestrusIT {
     @Test
     public void workerControllerIT() {
 
+        ResponseEntity<Worker> re1 = trt.postForEntity(baseUri + "worker/", w1, Worker.class);
+        assertEquals(HttpStatus.CREATED, re1.getStatusCode());
+
+        re1 = trt.postForEntity(baseUri + "worker/", w2, Worker.class);
+        assertEquals(HttpStatus.CREATED, re1.getStatusCode());
+        re1 = trt.postForEntity(baseUri + "worker/", w3, Worker.class);
+        assertEquals(HttpStatus.CREATED, re1.getStatusCode());
+
+        ResponseEntity<ArrayList> re2 = trt.getForEntity(baseUri + "workers/", ArrayList.class);
+        ArrayList a = new ArrayList();
+        a.add(w1);
+        a.add(w2);
+        a.add(w3);
+        assertEquals(a, re2.getBody());
+
+        ResponseEntity<Worker> re3 = trt.getForEntity(baseUri + "worker?id=" + w3.getId(), Worker.class);
+        assertEquals(w3, re3.getBody());
+
+        trt.delete(baseUri + "worker/" + w3.getId());
+        re2 = trt.getForEntity(baseUri + "workers/", ArrayList.class);
+        assertEquals(2, re2.getBody().size());
+
     }
 
     @Test
     public void imageControllerIT() {
 
+        ResponseEntity<DockerImage> re1 = trt.postForEntity(baseUri + "image/", i1, DockerImage.class);
+        assertEquals(HttpStatus.CREATED, re1.getStatusCode());
+
+        re1 = trt.postForEntity(baseUri + "image/", i2, DockerImage.class);
+        assertEquals(HttpStatus.CREATED, re1.getStatusCode());
+        re1 = trt.postForEntity(baseUri + "image/", i3, DockerImage.class);
+        assertEquals(HttpStatus.CREATED, re1.getStatusCode());
+
+        ResponseEntity<ArrayList> re2 = trt.getForEntity(baseUri + "images/", ArrayList.class);
+        ArrayList a = new ArrayList();
+        a.add(i1);
+        a.add(i2);
+        a.add(i3);
+        assertEquals(a, re2.getBody());
+
+        ResponseEntity<DockerImage> re3 = trt.getForEntity(baseUri + "image/" + i3.getId(), DockerImage.class);
+        assertEquals(i3, re3.getBody());
+
+        trt.delete(baseUri + "image/" + i3.getId());
+        re2 = trt.getForEntity(baseUri + "images/", ArrayList.class);
+        assertEquals(2, re2.getBody().size());
+
+        ResponseEntity<BasicResponse> re4 = trt.postForEntity(baseUri + "image?file=", i1, BasicResponse.class);
+        assertEquals(HttpStatus.NOT_IMPLEMENTED, re4.getBody());
+
+        ResponseEntity<BasicResponse> re5 = trt.postForEntity(baseUri + "image/start", i1, BasicResponse.class);
+        assertEquals(HttpStatus.OK, re5.getBody());
+
+        ResponseEntity<BasicResponse> re6 = trt.postForEntity(baseUri + "image/stop", i1, BasicResponse.class);
+        assertEquals(HttpStatus.OK, re6.getBody());
+
     }
 
-    @Test
-    public void clientControllerIT() {
-
-    }
-
-    @Test
-    public void dbInterfaceControllerIT() {
-
-    }
 }
