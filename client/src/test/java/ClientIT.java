c1b818c8075700af8aa66fea50ddd8f6c64d2286
Fiirouzz <farahreb@hotmail.fr>
2020-04-19 16:37:26 -0400

    Workers Tests

--- client/src/test/java/ClientIT.java
+++ client/src/test/java/ClientIT.java
@@ -1,7 +1,13 @@
 import ca.usherbrooke.dinf.client.ClientApplication;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(classes = ClientApplication.class)
 public class ClientIT {
+
+    @Test
+    public void restConsumerIT() {
+
+    }
 
 }
